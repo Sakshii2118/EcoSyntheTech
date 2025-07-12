e8273146c155e1b3e5dbb275fd0fe8e38d74e00f
Ananya Giri <ananyagiri257@gmail.com>
2025-07-12 14:24:33 +0530

    Added 4 files and made changes in 2

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,13 @@ app.use(express.json());
 // Routes (we will create them in later steps)
 app.use('/api/bookings', require('./routes/bookingRoutes'));
 app.use('/api/payments', require('./routes/paymentRoutes'));
+app.use('/api/auth', require('./routes/authRoutes'));
+const userRoutes = require('./routes/userRoutes');
+
+// after other middlewares
+app.use('/api/users', userRoutes);
+
+
 
 // Start Server
 const PORT = process.env.PORT || 5000;
